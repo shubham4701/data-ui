535a51e626388560d167f69c5e52767ff8a78ac1
shubham4701 <shubham_4701@live.com>
2023-04-18 11:48:20 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LocationSelector } from './location-selector/location-selector.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'find-by-pincode', component: AppComponent},
+  {path: 'find-by-city', component: LocationSelector},
+  {path: '', redirectTo: '/find-by-pincode', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
